b7be6cb0822145ea9f3242542f87fbb67ca1d17b
Shiran Abbasi <shiran.abbasi@gmail.com>
2025-01-04 19:18:36 +0500

    added order and pinned status to repo objects

--- js/script.js
+++ js/script.js
@@ -11,25 +11,33 @@ const app = createApp({
                 name: "vuejs-collapseable-side-menu",
                 description: "A collapsable side menu in vue js",
                 isPublic: true,
-                language: "JavaScript"
+                language: "JavaScript",
+                order: 4,
+                pinned: true
             },
             {
                 name: "tic-tac-toe",
                 description: "A tic tac toe in react js",
                 isPublic: true,
-                language: "JavaScript"
+                language: "JavaScript",
+                order: 2,
+                pinned: true
             },
             {
                 name: "nasa-react-app",
                 description: "A nasa picture of the day app",
                 isPublic: true,
-                language: "JavaScript"
+                language: "JavaScript",
+                order: 3,
+                pinned: true
             },
             {
                 name: "react-rodolist",
                 description: "A todo list in react js",
                 isPublic: true,
-                language: "JavaScript"
+                language: "JavaScript",
+                order: 1,
+                pinned: false
             }
         ];
         return {
